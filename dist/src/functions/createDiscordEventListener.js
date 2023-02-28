6c6ac2644ecafa623ed4c7512607a13f89363f6d
Bekian <47157992+Bekian@users.noreply.github.com>
2023-02-28 15:28:22 -0600

    removed dist dir

--- dist/src/functions/createDiscordEventListener.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createDiscordEventListener = void 0;
-function createDiscordEventListener(name, listener) {
-    return listener;
-}
-exports.createDiscordEventListener = createDiscordEventListener;
